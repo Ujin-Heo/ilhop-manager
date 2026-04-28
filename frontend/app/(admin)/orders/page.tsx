00f9da3faedd3ac770dbbef80eb5938844639a1a
howtojin <hujin2005@korea.ac.kr>
2026-04-28 22:45:28 +0900

    feat: Set up frontend project structure

---
+++ frontend/app/(admin)/orders/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <main>This is the orders-dashboard page.</main>;
+}
---
+++ frontend/app/(admin)/tables/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <main>This is the tables dashboard page.</main>;
+}
