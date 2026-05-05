1160ca41362d7f4d580fbbd9310dd505a67acafb
howtojin <hujin2005@korea.ac.kr>
2026-05-06 00:23:45 +0900

    feat: Change admin routes to use /admin in their paths

--- frontend/app/admin/(desktop)/layout.tsx
+++ frontend/app/admin/(desktop)/layout.tsx
@@ -1,9 +1,9 @@
 import NavigationBar from "@/components/admin/navigation-bar";
 
 const links = [
-  { name: "테이블 현황", href: "/tables" },
+  { name: "테이블 현황", href: "/admin/tables" },
 
-  { name: "주문 현황", href: "/orders" },
+  { name: "주문 현황", href: "/admin/orders" },
 ];
 
 export default function AdminLayout({
